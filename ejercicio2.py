c92c97aec096145e4bf7bcef6a678e04fb8a0cca
MarianKellyGM <108492184+MarianKellyGM@users.noreply.github.com>
2022-07-04 15:52:06 -0500

    Add files via upload

---
+++ ejercicio2.py
@@ -0,0 +1,9 @@
+#ejercicio 2 - Marian Kelly Guzman Mares 
+print("\tMaterias:")
+materias=["Matematicas","Fisica","Quimica","Historia","Lengua"]
+print("\t1.",materias[-5])#imprimir por elemento 
+print("\t2.",materias[-4])
+print("\t3.",materias[2])
+print("\t4.",materias[3])
+print("\t5.",materias[-1])
+print("\t Tira completa de materias",materias) #imprimir la lista completa
